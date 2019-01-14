abe764ccc21ee432606e4062bb93c1e30262d6dd
John Arkema <john.arkema@gmail.com>
2019-01-14 13:50:39 -0600

    LandingPage layout / navigation comp

---
+++ src/components/Navigation.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="Navigation">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    signIn() {
+
+    },
+    signOut() {
+
+    },
+  }
+}
+</script>
+<style scoped lang="scss">
+  
+</style>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,10 +1,48 @@
 <template>
   <div class="LandingPage">
-    <b-container>
-      <b-row>
-        <b-col>
-          <h1>Landing Page</h1>
-        </b-col>
+    <b-container fluid>
+      <!--  -->
+      <b-row no-gutters class="header" >
+       <b-col>
+        <b-jumbotron>
+          <template slot="header">
+            Bootstrap Vue
+          </template>
+          <template slot="lead">
+            This is a simple hero unit, a simple jumbotron-style component for
+            calling extra attention to featured content or information.
+          </template>
+          <hr class="my-4">
+          <p>
+            It uses utility classes for typography and spacing to space content
+            out within the larger container.
+          </p>
+          <div class="tronButtons">
+            <b-btn variant="primary" href="#">Do Something</b-btn>
+            <b-btn variant="success" href="#">Do Something Else</b-btn>
+          </div>
+        </b-jumbotron>
+       </b-col>
+      </b-row>
+      <!--  -->
+      <b-row class="pricingTable">
+        <b-col></b-col>
+      </b-row>
+      <!--  -->
+      <b-row class="imageSlider">
+        <b-col></b-col>
+      </b-row>
+      <!--  -->
+      <b-row class="clients">
+        <b-col></b-col>
+      </b-row>
+      <!--  -->
+      <b-row class="connect">
+        <b-col></b-col>
+      </b-row>
+      <!--  -->
+      <b-row class="footerMenu">
+        <b-col></b-col>
       </b-row>
     </b-container>
   </div>
@@ -12,5 +50,13 @@
 <script>
 </script>
 <style scoped lang="scss">
-  
+  // header
+  .container-fluid {
+    padding: 0;
+  }
+  .header {
+    .tronButtons .btn {
+      margin-left: 10px;
+    }
+  }
 </style>
