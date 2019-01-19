da4dccbc407bce40f8428e701f340a216004fd30
John Arkema <john.arkema@gmail.com>
2019-01-19 16:06:12 -0600

    Ellipse icon

--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -2,9 +2,12 @@ import Vue from 'vue';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faFolderOpen, faFileUpload, faFile } from '@fortawesome/free-solid-svg-icons';
+import {
+  faFolderOpen, faFileUpload, faFile, faEllipsisH,
+} from '@fortawesome/free-solid-svg-icons';
 
 library.add(faFolderOpen);
 library.add(faFileUpload);
 library.add(faFile);
+library.add(faEllipsisH);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
