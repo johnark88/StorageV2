51110144ecb2268ae25ed8675b7aa4bd3569487b
John Arkema <john.arkema@gmail.com>
2019-01-22 15:50:40 -0600

    add file display vue

---
+++ src/views/FileDisplay.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="FileDisplay">
+    <h3>Hello File Dispaly</h3>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped lang="scss">
+  
+</style>
